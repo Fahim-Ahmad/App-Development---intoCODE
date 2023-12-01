166aaeaad2e13918180d849904af036d108d7134
fahim <fyousufzai93@gmail.com>
2023-12-01 12:38:14 +0430

    used <Link> from 'react-router-dom' to link the logo to the main page instead of <a> tag

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import "./App.css";
 import recipeLogo from './assets/logo.svg'
 import { RecipeItemList } from "./components/RecipeItemList";
@@ -9,15 +9,13 @@ import {RecipeDetails} from "./components/RecipeDetails";
 function App() {
   return (
     <>
-      <div>
-        <a href="#" target="_blank">
-          <img src={recipeLogo} className="logo" alt="Recipe logo" />
-        </a>
-        <p className="display-15 text-center">Welcome to myRecipe Application</p>
-        <hr class="bg-success border-1 border-bottom border-success" />
-      </div>
-
     <BrowserRouter>
+        <Link to="/">
+          <img src={recipeLogo} className="logo" alt="Recipe logo" />
+        </Link>
+        <p className="display-15 text-center">Welcome to myRecipe Application</p>
+        <hr className="bg-success border-1 border-bottom border-success" />
+
       <Routes>
         <Route path = "/recipes/new" element={<NewRecipeForm />}/>
         <Route path="*" element={<RecipeItemList />} />
